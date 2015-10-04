023828db77192dc5089ecd5c1a6c711dd80c2077
mrlachance <mrlachance@laurentian.ca>
2015-10-04 15:37:13 -0400

    Fixed camera 2

--- Charlie Brown's Dragon Takedown/Assets/camera_controller_2.cs
+++ Charlie Brown's Dragon Takedown/Assets/camera_controller_2.cs
@@ -1,42 +1,38 @@
-﻿//Fov Cam
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class camera_controller_2 : MonoBehaviour {
-    //min and maximums for fov and fov start
-    float minFov = 50f;     
-    float maxFov = 200f;
-    float fov = 50f;
 
-    //speed for the scrolling 
-    //is negative for proper and not inverted scrolling
-    float sensitivity = -50f;
-    
     // Use this for initialization
     void Start () { }   
 	
     // Update is called once per frame
 	void Update () {
-        //x and y
+        float zMin = 25f;
+        float zMax = 150f;
+
+        //x, y, z movement
         float xAxisValue = Input.GetAxis("Horizontal");
         float yAxisValue = Input.GetAxis("Vertical");
-        
-        //fov with scroll wheel
-        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        float zAxisValue = Input.GetAxis("Mouse ScrollWheel") * 20f;//*10: Increase speed
 
-        //fov with page up/down
+        //z movement:page up, page down
         if (Input.GetKey(KeyCode.PageUp))
-            fov += 1.5f;
+            zAxisValue += 1.5f;
         else if (Input.GetKey(KeyCode.PageDown))
-            fov -= 1.5f;
-
-        //min <= fov <= max
-        fov = Mathf.Clamp(fov, minFov, maxFov);
-       
-        //moving the camera camera
-        if (Camera.current != null) {
-            Camera.current.transform.Translate(new Vector3(xAxisValue, yAxisValue, 0.0f));
-            Camera.main.fieldOfView = fov;
+            zAxisValue -= 1.5f;
+        Debug.Log(Camera.main.transform.position);
+        
+        //moving the camera
+        if (Camera.main != null)
+        {
+            //the y for the camera is the z for the world
+            if ((Camera.main.transform.position.y >= zMax) && (zAxisValue < 0))
+                Camera.main.transform.Translate(new Vector3(xAxisValue, yAxisValue, 0)); 
+            else if ((Camera.main.transform.position.y <= zMin) && (zAxisValue > 0)) 
+                Camera.main.transform.Translate(new Vector3(xAxisValue, yAxisValue, 0));
+            else
+                Camera.main.transform.Translate(new Vector3(xAxisValue, yAxisValue, zAxisValue));
         }
     }
 }
